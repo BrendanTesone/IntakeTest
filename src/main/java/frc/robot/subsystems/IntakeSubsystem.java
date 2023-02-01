ff566b3168c03fa0f8f49ce3d74c4267bc75042a
dtesone <96949900+dtesone@users.noreply.github.com>
2023-01-31 19:48:36 -0500

    commit

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import frc.lib.AftershockSubsystem;
+
+import frc.robot.Constants.PortConstants;
+import com.revrobotics.CANSparkMax;
+public class IntakeSubsystem extends AftershockSubsystem {
+    //@author
+    //TODO: test motor with this code, add "brushless" motor type to sparkmax object
+    //TODO: add constants for motor speed for all 4 situations
+    CANSparkMax spark = new CANSparkMax(PortConstants.kSparkMaxPort, null); // 0 is the RIO PWM port this is connected to
+
+    public IntakeSubsystem(){
+        super();
+    }
+
+    //Turn on the motor to input the cone
+    //Button right trigger
+    //clockwise to output
+    public void intakeCone(){
+        spark.set(-0.75);
+    }
+    //Turn on the motor to output the cone
+    //Button left trigger
+    //counterclockwise to intake
+    public void outputCone(){
+        
+    }
+    //Turn on the motor to input the cube
+    //Button right bumper
+    //counterclockwise to input
+    public void intakeCube(){
+
+    }
+    //Turn on the motor to output the cube
+    //Button left bumper
+    //clockwise to output
+    public void outputCube(){
+        
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        
+    }
+
+//right trigger, left trigger - cones
+//right bumper left bumper - cubes
+//right for consume, left for expel
+
+    
+
+}
